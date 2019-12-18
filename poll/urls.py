58934cea45a72cd892910a03e139927eccaadcaa
jeongmu <jeongmu.park@medit.com>
2019-12-19 00:03:10 +0900

    3.2.14 - 제네릭 뷰 사용하기(클래스형 뷰)

--- poll/urls.py
+++ poll/urls.py
@@ -6,18 +6,31 @@ from . import views
 # 이 네임스페이스를 사용하기 위해서 템플릿에도 수정을 해야함.
 app_name = 'poll'
 
+# 클래스형 뷰로 변경.
+# 아래 함수형 뷰와 다른 점은,
+# route 패턴에 들어있는 패턴이름과 view 인자
+# 패턴이름을 question_id에서 pk로 변경했음. 그리고 뷰 이름을 바꾸고 뒤에 추가 코드를 붙임.
+# 함수형 뷰를 사용할 떄는 뷰를 그대로 써주면 되지만 클래스형 뷰를 사용할 떄는 꼭 뒤에 [as_view()]를 붙여야 하기 때문임.
 urlpatterns = [
-    #poll/views.py가 작동하도록 아래에 URL을 연결
-
-    # ex: /poll/
-    path('', views.index, name='index'),
-
-    # ex: /poll/5/
-    path('<int:question_id>/', views.detail, name='detail'),    #<>(화살괄호)는 변수를 의미
-
-    # ex: /poll/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-
-    # ex: /poll/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote')
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name="results"),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
+
+# 함수형 뷰
+# urlpatterns = [
+#     #poll/views.py가 작동하도록 아래에 URL을 연결
+#
+#     # ex: /poll/
+#     path('', views.index, name='index'),
+#
+#     # ex: /poll/5/
+#     path('<int:question_id>/', views.detail, name='detail'),    #<>(화살괄호)는 변수를 의미
+#
+#     # ex: /poll/5/results/
+#     path('<int:question_id>/results/', views.results, name='results'),
+#
+#     # ex: /poll/5/vote/
+#     path('<int:question_id>/vote/', views.vote, name='vote')
+# ]
--- poll/views.py
+++ poll/views.py
@@ -2,30 +2,49 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.urls import reverse
 from django.template import loader  #템플릿을 불러오기 위해 loader 임포트
 from django.shortcuts import render, get_object_or_404
+from django.views import generic
 
 from .models import Question, Choice
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published question"""
+        return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
+
 #인덱스 화면 출력
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]   #질문 리스트 5개만 가져오기
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]   #질문 리스트 5개만 가져오기
 
     # 1.render 메서드를 사용하지 않고 아래와 같이 코드 작성 가능
-    # template = loader.get_template('polls/index.html')  # .html 템플릿 연결하기
-    # context = { #latest_question_list(투표목록) 변수를 전달하기 위한 context
-    #     'latest_question_list':latest_question_list,
-    # }
-    # return HttpResponse(template.render(context, request))
+#    template = loader.get_template('polls/index.html')  # .html 템플릿 연결하기
+#    context = { #latest_question_list(투표목록) 변수를 전달하기 위한 context
+#       'latest_question_list':latest_question_list,
+#    }
+#     return HttpResponse(template.render(context, request))
 
     # 2.위 방법이 번거롭게 느껴진다면 render 메서드를 활용해서 아래와 같이 작성 가능
-    context = { 'latest_question_list': latest_question_list } # latest_question_list(투표목록) 변수를 전달하기 위한 context
-    return render(request, 'polls/index.html', context)
+#    context = { 'latest_question_list': latest_question_list } # latest_question_list(투표목록) 변수를 전달하기 위한 context
+#    return render(request, 'polls/index.html', context)
 
     # output = ', '.join([q.question_text for q in latest_question_list])
     # return HttpResponse(output)
 
 
 #투표 상세 화면 출력
-def detail(request, question_id): #특별한 결과 없이 값만 출력
+#def detail(request, question_id): #특별한 결과 없이 값만 출력
     # 1. 투표 상세 목록을 불러옴. 불러올 항목이 없을 경우 404 에러를 발생시킨다.
     # try:
     #     question = Question.objects.get(pk=question_id)
@@ -34,15 +53,15 @@ def detail(request, question_id): #특별한 결과 없이 값만 출력
     # return render(request, 'polls/detail.html', {'question':question})
 
     # 2. get_object_or_404 메서드를 활용해 위의 코드를 간소화 시킬 수 있다.
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question':question})
+ #   question = get_object_or_404(Question, pk=question_id)
+ #   return render(request, 'polls/detail.html', {'question':question})
 
     # return HttpResponse("You're looking at question %s." % question_id)
 
 #투표 결과 화면 출력
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question':question})
+#def results(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/results.html', {'question':question})
 
 #투표 기능 화면 출력
 def vote(request, question_id): #특별한 결과 없이 값만 출력
