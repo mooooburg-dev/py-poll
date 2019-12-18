f8ec308199700ef5c49e6545bd079756a389de6d
jeongmu <jeongmu.park@medit.com>
2019-12-18 15:34:11 +0900

    3.2.9 URL 연결

--- poll/views.py
+++ poll/views.py
@@ -1,13 +1,16 @@
 from django.http import HttpResponse
 
-def detail(request, question_id):
+#투표 상세 화면 출력
+def detail(request, question_id): #특별한 결과 없이 값만 출력
     return HttpResponse("You're looking at question %s." % question_id)
 
-def results(request, question_id):
+#투표 결과 화면 출력
+def results(request, question_id):  #특별한 결과 없이 값만 출력
     response = "You're looking at the resules of question %s."
     return HttpResponse(response % question_id)
 
-def vote(request, question_id):
+#투표 기능 화면 출력
+def vote(request, question_id): #특별한 결과 없이 값만 출력
     return HttpResponse("You're voting on quesion %s." % question_id)
 
 def index(request):
