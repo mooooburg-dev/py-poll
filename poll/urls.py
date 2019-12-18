f421f4be98570c9edc5a1d9777e3bfe6d814b65f
jeongmu <jeongmu.park@medit.com>
2019-12-18 22:41:04 +0900

    3.2.12 - URL 네임스페이스(Namespace) 설정하기

--- poll/urls.py
+++ poll/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+# 네임스페이스 설정
+# urls.py에 [app_name]이라는 변수를 추가하고 이름을 설정하면 네임스페이스 설정은 완료.
+# 이 네임스페이스를 사용하기 위해서 템플릿에도 수정을 해야함.
+app_name = 'poll'
+
 urlpatterns = [
     #poll/views.py가 작동하도록 아래에 URL을 연결
 
