1a1a2e1099dadc3acfb8a68345ec177a0dac2dd8
jeongmu <jeongmupark@gmail.com>
2019-12-19 17:22:11 +0900

    3.2.17 관리자 화면 목록 커스터마이징 중간 추가(py-poll 최종)

--- poll/admin.py
+++ poll/admin.py
@@ -1,3 +1,6 @@
+#관리자 페이지의 디자인을 변경하고 싶다면,
+#django/contrib/admin/templates의 템플릿을 복사해 프로젝트로 가져와 수정할 수 있음
+
 from typing import Any
 
 from django.contrib import admin
@@ -6,12 +9,13 @@ from .models import Question, Choice
 # 관리자 화면을 커스터마이징 하기 위해서는 admin.py에서 Model Admin을 상속받는 클래스를 만들고 register에 두 번째 인자로 전달해야 한다.
 
 # 답변 항목도 같이 등록 및 수정할 수 있도록 코드 추가(상단에 Choice 모델도 임포트)
-# [StackedInline] 클래스를 상속 받음. 그리고 이 클래스를 [QuestionAdmin] 클래스의 [inlines] 클래스 변수에 추가함(19Line)
-#선택하는 방식-1
+# [StackedInline 또는 TabularInline] 클래스를 상속 받음. 그리고 이 클래스를 [QuestionAdmin] 클래스의 아래 [inlines] 클래스 변수에 추가함
+#선택하는 방식 - 1.StackedInline
 # class ChoiceInline(admin.StackedInline):
 #     model = Choice
 #     extra = 3
-# 선택하는 방식-2
+
+# 선택하는 방식 - 2.TabularInline
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
@@ -27,6 +31,14 @@ class QuestionAdmin(admin.ModelAdmin):
     # 변수의 값은 튜플로 출력하고 싶은 항목을 묶어서 설정.
     list_display = ('question_text', 'pub_date', 'was_published_recently')
 
+    # Stacked 또는 Tabular Inline을 상속받은 ChoiceInline 클래스를 inlines 변수에 추가
     inlines = [ChoiceInline]
+
+    # 검색 및 필터 기능 추가
+    # list_filter, search_field 클래스 변수를 추가하여 사용할 수 있음
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
+
+
 
 admin.site.register(Question, QuestionAdmin)
